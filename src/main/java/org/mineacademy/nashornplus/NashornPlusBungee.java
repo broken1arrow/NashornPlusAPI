9bbd9cde249b6567e4b8bdc51274923804558373
broken1arrow <broken2arrow1@gmail.com>
2022-05-27 20:43:16 +0200

    Initial commit

---
+++ src/main/java/org/mineacademy/nashornplus/NashornPlusBungee.java
@@ -0,0 +1,41 @@
+package org.mineacademy.nashornplus;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+import javax.script.ScriptEngineFactory;
+import javax.script.ScriptEngineManager;
+
+public final class NashornPlusBungee extends Plugin {
+
+	@Override
+	public void onEnable() {
+
+		//engineManager = new ScriptEngineManager();
+		// engineFactory = new org.openjdk.nashorn.api.scripting.NashornScriptEngineFactory();
+
+		// Register Nashorn library
+		// engineManager.registerEngineName("Nashorn", engineFactory);
+
+		//
+		// Example usage
+		//
+
+		/*final javax.script.ScriptEngine engine = engineManager.getEngineByName("Nashorn");
+
+		// Run JavaScript code as usual
+		try {
+			engine.eval("print('Hello World!');");
+
+		} catch (final javax.script.ScriptException ex) {
+			ex.printStackTrace();
+		}*/
+	}
+
+	public ScriptEngineManager getEngineManager() {
+		return null;
+	}
+
+	public ScriptEngineFactory getEngineFactory() {
+		return null;
+	}
+}
